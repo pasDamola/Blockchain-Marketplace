ed55a7ff9f7b8efb4def1439b4578306691d24ef
oyincode <oyindadavid18@gmail.com>
2019-09-03 21:29:15 +0100

    purchase product function

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -7,7 +7,7 @@ contract Marketplace {
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -17,7 +17,15 @@ contract Marketplace {
     uint id,
     string name,
     uint price,
-    address owner,
+    address payable owner,
+    bool purchased
+);
+
+event ProductPurchased(
+    uint id,
+    string name,
+    uint price,
+    address payable owner,
     bool purchased
 );
     constructor() public {
@@ -37,6 +45,31 @@ contract Marketplace {
     // Trigger an event
     emit ProductCreated(productCount, _name, _price, msg.sender, false);
 }
+
+  function purchaseProduct(uint _id) public payable {
+      // Fetch the product
+    Product memory _product = products[_id];
+    // Fetch the owner
+    address payable _seller = _product.owner;
+    // Make sure the product has a valid id
+    require(_product.id > 0 && _product.id <= productCount, "product must have a valid id");
+    // Require that there is enough Ether in the transaction
+    require(msg.value >= _product.price, "not enough ether to perform purchase");
+    // Require that the product has not been purchased already
+    require(!_product.purchased, "product has already been purchased");
+    // Require that the buyer is not the seller
+    require(_seller != msg.sender, "buyer must not be seller");
+    // Transfer ownership to the buyer
+    _product.owner = msg.sender;
+    // Mark as purchased
+    _product.purchased = true;
+    // Update the product
+    products[_id] = _product;
+    // Pay the seller by sending them Ether
+    address(_seller).transfer(msg.value);
+    // Trigger an event
+    emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+  }
 
 
 }
