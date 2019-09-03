85593d91d817b585f03d5d20e42f38fd1e3d8906
oyincode <oyindadavid18@gmail.com>
2019-09-03 19:18:05 +0100

    Product created Event

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -13,9 +13,30 @@ contract Marketplace {
 
   mapping(uint => Product) public products;
 
+  event ProductCreated(
+    uint id,
+    string name,
+    uint price,
+    address owner,
+    bool purchased
+);
     constructor() public {
         name = "Oyincode Marketplace";
     }
+
+
+    function createProduct(string memory _name, uint _price) public {
+    // Require a valid name
+    require(bytes(_name).length > 0, "length of product name must not be null");
+    // Require a valid price
+    require(_price > 0, "price of product must not be null");
+    // Increment product count
+    productCount ++;
+    // Create the product
+    products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+    // Trigger an event
+    emit ProductCreated(productCount, _name, _price, msg.sender, false);
+}
 
 
 }
