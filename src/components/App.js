766bf03be956286dc213223811cffa27a559b43a
oyincode <oyindadavid18@gmail.com>
2019-09-03 23:07:39 +0100

    initial state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
 
 class App extends Component {
+  async componentWillMount (){
+    await this.loadWeb3();
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3 () {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts);
+    this.setState({ account: accounts[0] })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
+    }
+  }
+
+
   render() {
-    return (
+   return (
       <div>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
