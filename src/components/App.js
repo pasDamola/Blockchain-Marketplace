ca66eb877044c60fda75b332ba4145a562b94d69
oyincode <oyindadavid18@gmail.com>
2019-09-04 12:09:29 +0100

    deployed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import Web3 from 'web3'
 import logo from '../logo.png';
 import './App.css';
-import Web3 from 'web3';
-import Navbar from './Navbar';
-import Marketplace from '../abis/Marketplace.json';
+import Marketplace from '../abis/Marketplace.json'
+import Navbar from './Navbar'
 import Main from './Main'
 
 class App extends Component {
-  async componentWillMount (){
-    await this.loadWeb3();
+
+  async componentWillMount() {
+    await this.loadWeb3()
     await this.loadBlockchainData()
   }
 
-  async loadWeb3 () {
+  async loadWeb3() {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum)
       await window.ethereum.enable()
@@ -31,25 +32,36 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
     const networkId = await web3.eth.net.getId()
-    console.log(networkId);
     const networkData = Marketplace.networks[networkId]
     if(networkData) {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
+      const productCount = await marketplace.methods.productCount().call()
+      this.setState({ productCount })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
       this.setState({ loading: false})
     } else {
       window.alert('Marketplace contract not deployed to detected network.')
     }
+  }
 
-    const productCount = await this.state.marketplace.methods.productCount().call()
-    this.setState({ productCount })
-    // Load products
-    for (var i = 1; i <= productCount; i++) {
-      const product = await this.state.marketplace.methods.products(i).call()
-      this.setState({
-        products: [...this.state.products, product]
-      })
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true
     }
+
+    this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
   createProduct(name, price) {
@@ -68,33 +80,24 @@ class App extends Component {
     })
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      productCount: 0,
-      products: [],
-      loading: true
-    }
-    this.createProduct = this.createProduct.bind(this)
-  }
-
-
   render() {
-   return (
-    <div>
-    <Navbar account={this.state.account} />
-    <div className="container-fluid mt-5">
-      <div className="row">
-      <main role="main" className="col-lg-12 d-flex">
-        { this.state.loading
-          ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-          : <Main createProduct={this.createProduct} />
-        }
-     </main>
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex">
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <Main
+                  products={this.state.products}
+                  createProduct={this.createProduct}
+                  purchaseProduct={this.purchaseProduct} />
+              }
+            </main>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     );
   }
 }
